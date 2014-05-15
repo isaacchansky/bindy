b18fd9d50dc06b0ebeb3d29caae1f2f6756ca8ab
Isaac Chansky <ichansky@gmail.com>
2014-05-14 21:24:56 -0400

    working on initial binding

--- jquery.bindy.js
+++ jquery.bindy.js
@@ -8,17 +8,54 @@
 *
 */
 
-(function( $ ){
+// the semi-colon before function invocation is a safety net against concatenated
+// scripts and/or other plugins which may not be closed properly.
+;(function($, window, document, undefined ){
   "use strict";
 
   $.fn.bindy = function( options ) {
+    console.log('yay bindy!');
     var settings = {
     };
+    options = options || {};
+    $.extend( settings, options );
 
-    if ( options ) {
-      $.extend( settings, options );
+    var scope = options.scope || {};
+
+    function updateModel(e){
+      var $target = $(e.currentTarget);
+      var binding = $target.data().bindTo.split('.');
+      var object = binding[0];
+      var attr = binding[1];
+      if(scope[object] === undefined){
+        scope[object] = {};
+      }
+      scope[object][attr] = $target.val();
+      console.log('scope:');
+      console.log(scope);
     }
 
+    function updateDOMbindings(e){
+      var $target = $(e.currentTarget);
+      var binding = $target.data().bindTo.split('.');
+      var object = binding[0];
+      var attr = binding[1];
+      var domBindings = $('[data-bind-from]');
+
+      domBindings.each(function(i, el){
+        var binder = $(el).data().bindFrom.split('.');
+        var object = binder[0];
+        var attr = binder[1];
+        var value = scope[object][attr];
+        $(el).html(value);
+      });
+    }
+
+    var $dataBindings = $('[data-bind-to]');
+
+    $dataBindings.on('change', updateModel);
+    $dataBindings.on('change', updateDOMbindings);
 
   };
-})( window.jQuery || window.$ );
+
+})(jQuery, window, document );
